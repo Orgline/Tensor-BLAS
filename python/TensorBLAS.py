cad20a91eb9dcecfb3541faea44ffb4008020d45
Orgline <Orgline@outlook.com>
2023-06-04 21:41:44 +0800

    Update TensorBLAS.py
    Update tc_syrk_NT routine, assume C=A*A.t

--- python/TensorBLAS.py
+++ python/TensorBLAS.py
@@ -6,34 +6,64 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 lib = ctypes.CDLL(os.path.join(dir_path, '../build/TensorBLAS.so'))
 
-n = 20000
-k = 40000
-nb = 256
+'''
+C=A*A.t
+'''
+def tc_syrk_NT(A, C):
 
-print_env = lib.print_env
-print_env()
-
-lib.tc_syrk_wrapper.argtypes = [
+    # Get n, k from A
+    n = A.size(0)
+    k = A.size(1)
+    nb = 256
+    lib.tc_syrk_wrapper.argtypes = [
     ctypes.c_long,
     ctypes.c_long,
     ctypes.POINTER(ctypes.c_float),
     ctypes.POINTER(ctypes.c_float),
     ctypes.c_long
-]
-lib.tc_syrk_wrapper.restype = ctypes.c_int
+    ]
+    lib.tc_syrk_wrapper.restype = ctypes.c_int
+    ptr_A = ctypes.cast(A.data_ptr(), ctypes.POINTER(ctypes.c_float))
+    ptr_C = ctypes.cast(C.data_ptr(), ctypes.POINTER(ctypes.c_float))
+
+    # Call tc_syrk_wrapper
+    status = lib.tc_syrk_wrapper(ctypes.c_long(n), ctypes.c_long(k), ptr_A, ptr_C, ctypes.c_long(nb))
+    assert status == 0
+
+def test_tc_syrk_NT(A, C)
+    tc_syrk_NT(A, C)
+    print(torch.linalg.norm(A @ A.t()-C)/torch.linalg.norm(C))
+
+
+
+
+n = 20000
+k = 40000
+nb = 256
+
+# print_env = lib.print_env
+# print_env()
+
+# lib.tc_syrk_wrapper.argtypes = [
+#     ctypes.c_long,
+#     ctypes.c_long,
+#     ctypes.POINTER(ctypes.c_float),
+#     ctypes.POINTER(ctypes.c_float),
+#     ctypes.c_long
+# ]
+# lib.tc_syrk_wrapper.restype = ctypes.c_int
 
 A = torch.randn(n, k).cuda()
 C = torch.zeros(n, n, device='cuda', dtype=torch.float32)
-ptr_A = ctypes.cast(A.data_ptr(), ctypes.POINTER(ctypes.c_float))
-ptr_C = ctypes.cast(C.data_ptr(), ctypes.POINTER(ctypes.c_float))
+test_tc_syrk_NT(A, C)
+# ptr_A = ctypes.cast(A.data_ptr(), ctypes.POINTER(ctypes.c_float))
+# ptr_C = ctypes.cast(C.data_ptr(), ctypes.POINTER(ctypes.c_float))
 
-status = lib.tc_syrk_wrapper(ctypes.c_long(n), ctypes.c_long(k), ptr_A, ptr_C, ctypes.c_long(nb))
-assert status == 0
+# status = lib.tc_syrk_wrapper(ctypes.c_long(n), ctypes.c_long(k), ptr_A, ptr_C, ctypes.c_long(nb))
+# assert status == 0
 #print(A)
 #print(AA)
 #A = torch.tensor([[1.0, 2, 3], [4, 5, 6]]).cuda()
 #print(C)
 #print(AA @ AA.t())
-print(torch.linalg.norm(A @ A.t()-C)/torch.linalg.norm(C))
-
-
+#print(torch.linalg.norm(A @ A.t()-C)/torch.linalg.norm(C))
