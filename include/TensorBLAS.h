891b8f9726ccfe9e05d2b486ef953943d582dad0
z52527 <zhaorunchu@gmail.com>
2023-06-06 15:25:55 +0900

    add tc_cumpsgemm_syrk testing_cumpsgemm_syrk

--- include/TensorBLAS.h
+++ include/TensorBLAS.h
@@ -8,6 +8,7 @@
 
 #include <cusolverDn.h>
 
+#include <cumpsgemm/cumpsgemm.hpp>
 
 __global__
 void s2h(long int m, long int n, float *as, long int ldas, __half *ah, long int ldah);
@@ -91,6 +92,8 @@ extern "C" __attribute__((visibility("default"))) void print_env();
 extern "C" __attribute__((visibility("default"))) int tc_syrk_wrapper(long int n, long int k, float* A, float* C, long int nb);
 
 extern "C" __attribute__((visibility("default"))) void tc_syrk(cublasHandle_t handle, long int n,long int k,  float alpha, float* A, long int lda, float beta, float* C, long int ldc, __half* Ah, long int nb);
+
+extern "C" __attribute__((visibility("default"))) void tc_cumpsgemm_syrk(cumpsgemm::handle_t handle, long int n,long int k,  float alpha, float* A, long int lda, float beta, float* C, long int ldc, long int nb);
 
 void tc_trsm(cublasHandle_t handle, long int m, long int n, float* A, long int lda, float* B, long int ldb, __half* hwork, long int nb);
 
